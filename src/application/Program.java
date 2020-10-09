c762129831142e9564f458d16316ebc5ff77b31d
Calebe Rodrigues <calebebrodrigues@gmail.com>
2020-10-09 17:18:20 -0300

    Projeto finalizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,47 @@
 package application;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import entities.Alunos;
+
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanner = new Scanner(System.in);
+		Set<Alunos> alunos = new HashSet();
+ 		
+		System.out.print("How many students course A? ");
+		int n = scanner.nextInt();
+		while(n > 0) {
+			int id = scanner.nextInt();
+			alunos.add(new Alunos(id));
+			n--;
+		}
+		
+		
+		System.out.print("How many students course B? ");
+		n = scanner.nextInt();
+		while(n > 0) {
+			int id = scanner.nextInt();
+			alunos.add(new Alunos(id));
+			n--;
+		}
+		
+		
+		System.out.print("How many students course C? ");
+		n = scanner.nextInt();
+		while(n > 0) {
+			int id = scanner.nextInt();
+			alunos.add(new Alunos(id));
+			n--;
+		}
+		
+		System.out.println("Total students: " + alunos.size());
+		scanner.close();
 	}
 
 }
--- src/entities/Alunos.java
+++ src/entities/Alunos.java
@@ -1,5 +1,50 @@
 package entities;
 
-public class Alunos {
+import java.util.ArrayList;
+import java.util.List;
 
+import entities.enums.Cursos;
+
+public class Alunos {
+	private int IDAluno;
+	private List<Cursos> cursos = new ArrayList<Cursos>();
+	
+	public Alunos() {
+		
+	}
+
+	public Alunos(int iDAluno) {
+		IDAluno = iDAluno;
+	}
+
+	public List<Cursos> getCursos() {
+		return cursos;
+	}
+
+	public void setCursos(Cursos cursos) {
+		this.cursos.add(cursos);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + IDAluno;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Alunos other = (Alunos) obj;
+		if (IDAluno != other.IDAluno)
+			return false;
+		return true;
+	}
+	
 }
